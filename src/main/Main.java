04a461f3e8d03a24c1b3f7f8c5dc03093a18d5f1
Valter Uotila <valter.uotila@gmail.com>
2018-12-22 21:23:45 +0200

    converting csv to xml

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,19 +2,24 @@ package main;
 
 import java.io.FileNotFoundException;
 import GUI.graphic;
+import csvtoxml.Csvtoxml;
+import java.io.File;
+import javax.xml.parsers.ParserConfigurationException;
 
 public class Main {
 
-	public static void main(String args[]) {
-		new graphic();
-		try {
-			xmlReader.Reader reader = new xmlReader.Reader("src/invoices/invoices1.xml");
-			reader.createTree("Invoices");
-			
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+    public static void main(String args[]) throws ParserConfigurationException {
+        new graphic();
+        try {
+            Csvtoxml converter = new Csvtoxml("Persons", "Person", new File("src\\persons\\Person.csv"), "Person.xml");
+            converter.convertCSVtoXML();
+            xmlReader.Reader reader = new xmlReader.Reader("src\\invoices\\invoices1.xml");
+            reader.createTree("Invoices");
+
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
 }
